87961643e6fc2bc6a9309c816f214e76d9426131
S-M-Jawad-Ahmed <sheikhmuhammadjawadahmed001@gmail.com>
2025-10-13 10:57:31 +0000

    Disable caching for development to ensure live updates are always seen
    Add cache-control headers to disable caching for all requests in the development environment by modifying server.js.

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
+// Disable caching for development
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
